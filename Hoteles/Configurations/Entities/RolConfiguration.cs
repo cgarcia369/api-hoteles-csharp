220884886b7a616859dcb57c8682fb4f6a4ca6df
Camilo Garcia <cgarcia369@misena.edu.co>
2021-11-03 13:20:46 -0500

    chore: se agrega semilla de datos , se agrega connection stringy se actualiza dbcontext

---
+++ Hoteles/Configurations/Entities/RolConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hoteles.Configurations.Entities
+{
+    public class RolConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+
+                new IdentityRole()
+                {
+                    Name = "Administrador",
+                    NormalizedName = "ADMINISTRATOR"
+                }
+                );
+        }
+    }
+}
--- Hoteles/Data/Context/DatabaseContext.cs
+++ Hoteles/Data/Context/DatabaseContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hoteles.Configurations.Entities;
 using Hoteles.Data.Models;
 
 namespace Hoteles.Data.Context
@@ -16,6 +17,14 @@ namespace Hoteles.Data.Context
         }
         public  DbSet<Country> Countries { get; set; }
         public  DbSet<Hotel> Hotels { get; set; }
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            base.OnModelCreating(model);
 
+            model.ApplyConfiguration(new CountryConfiguration());
+            model.ApplyConfiguration(new HotelConfigurations());
+            model.ApplyConfiguration(new RolConfiguration());
+   
+        }
     }
 }
