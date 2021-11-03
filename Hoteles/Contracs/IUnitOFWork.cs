67ad944c2409726a04ff3f3e4dfa96fa796510cc
Camilo Garcia <cgarcia369@misena.edu.co>
2021-11-03 14:25:51 -0500

    chore: se agrega paquete pagedlist , se realiza patron repository y unitofwork

---
+++ Hoteles/Contracs/IUnitOFWork.cs
@@ -0,0 +1,15 @@
+﻿using Hoteles.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hoteles.Contracs
+{
+    public interface IUnitOFWork : IDisposable
+    {
+        IRepository<Country> Countries { get; }
+        IRepository<Hotel> Hotels { get; }
+        Task Save();
+    }
+}
