75720ca50671bb9af08d6c359f3e5f1c16d74c32
Camilo Garcia <cgarcia369@misena.edu.co>
2021-11-03 14:49:56 -0500

    feat: se realiza controllador de country , con las acciones get y get by id

---
+++ Hoteles/Controllers/CountryController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Hoteles.Contracs;
+using Hoteles.Data.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Hoteles.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IUnitOFWork _unitOFWork;
+        private readonly ILogger<CountryController> _logger;
+        private readonly IMapper _mapper;
+
+        public CountryController(IUnitOFWork unitOFWork, ILogger<CountryController> logger, IMapper mapper)
+        {
+            _unitOFWork = unitOFWork;
+            _logger = logger;
+            _mapper = mapper;
+                
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            try
+            {
+                var countries = await _unitOFWork.Countries.GetAll();
+                var results = _mapper.Map<IList<CountryDTO>>(countries);
+                return Ok(results);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Something wen wrong in the {nameof(GetCountries)}");
+                return StatusCode(500, "Internal Server error, please try Again later.");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCountry(int id)
+        {
+            try
+            {
+                var country = await _unitOFWork.Countries.Get(c=>c.Id == id, new List<string>() {"Hotels"});
+                var result = _mapper.Map<CountryDTO>(country);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Something wen wrong in the {nameof(GetCountry)}");
+                return StatusCode(500, "Internal Server error, please try Again later."); ;
+            }
+        }
+    }
+}
