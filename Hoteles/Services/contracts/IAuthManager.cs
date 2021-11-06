678fbceb27027bdb0fbc6fbb2cded71e10bb0e96
Camilo Garcia <cgarcia369@misena.edu.co>
2021-11-06 15:18:01 -0500

    feat: se construye los servicios asociados a logear usuario y crear jwt token

---
+++ Hoteles/Services/contracts/IAuthManager.cs
@@ -0,0 +1,18 @@
+﻿using Hoteles.Data.DTOs;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Hoteles.Services.contracts
+{
+     public interface IAuthManager
+    {
+        Task<bool> ValidateUser(LoginUserDTO loginUserDTO);
+
+        Task<string> CreateToken();
+        
+    }
+}
