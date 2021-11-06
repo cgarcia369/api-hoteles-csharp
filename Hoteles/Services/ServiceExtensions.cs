d5d102c1e7edabb88d1304447b651d5f7d80e76b
Camilo Garcia <cgarcia369@misena.edu.co>
2021-11-06 15:13:33 -0500

    chore: se configura jwt

--- Hoteles/Services/ServiceExtensions.cs
+++ Hoteles/Services/ServiceExtensions.cs
@@ -5,7 +5,12 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Hoteles.Services
 {
@@ -16,6 +21,27 @@ namespace Hoteles.Services
             var builder = service.AddIdentityCore<ApiUser>(r => r.User.RequireUniqueEmail = true);
             builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), service);
             builder.AddEntityFrameworkStores<DatabaseContext>().AddDefaultTokenProviders();
+        }
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"])),
+
+                    };
+                });
         }
 
     }
